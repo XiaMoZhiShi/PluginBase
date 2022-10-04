8d8d2cd5ff2676046a7f642bb9d58d312c5a361c
MATRIX-feather <midnightcarnival@outlook.com>
2022-10-05 00:46:02 +0800

    enhance: 为ISubCommand实现默认onTabComplete方法 misc: 补全文档 misc: Bump version
    Signed-off-by: MATRIX-feather <midnightcarnival@outlook.com>

--- src/main/java/xiamomc/pluginbase/Command/ISubCommand.java
+++ src/main/java/xiamomc/pluginbase/Command/ISubCommand.java
@@ -4,24 +4,78 @@ import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public interface ISubCommand
 {
+    @NotNull
     public String getCommandName();
 
     @Nullable
     public default List<String> onTabComplete(List<String> args, CommandSender source)
     {
-        return null;
+        var subCommands = getSubCommands();
+        if (subCommands == null || subCommands.length == 0) return null;
+
+        var cmdBaseName = args.get(0);
+
+        //找找有没有符合条件的子命令
+        var cmd = Arrays.stream(subCommands)
+                .filter(c -> c.getCommandName().equals(cmdBaseName)).findFirst();
+        if (cmd.isPresent()) //如果有则让子命令补全
+        {
+            args.remove(0);
+            return cmd.get().onTabComplete(args, source);
+        }
+
+        //没有则列出所有可用命令
+        var list = new ArrayList<String>();
+
+        for (var c : subCommands)
+        {
+            var perm = c.getPermissionRequirement();
+
+            if (perm != null && !source.hasPermission(perm)) continue;
+
+            var name = c.getCommandName();
+
+            if (name.toLowerCase().startsWith(cmdBaseName.toLowerCase())) list.add(name);
+        }
+
+        return list;
     }
 
+    /**
+     * 获取权限要求
+     * @return 权限要求，若为null则没有要求
+     */
     @Nullable
     public default String getPermissionRequirement()
     {
         return null;
     }
 
+    /**
+     * 获取此指令的帮助信息
+     * @return 帮助信息
+     */
+    @Nullable
     public String getHelpMessage();
+
+    /**
+     * 获取此子指令下面可用的指令
+     * @return 指令列表
+     */
+    @Nullable
+    public default ISubCommand[] getSubCommands() { return null; }
+
+    /**
+     *
+     * @param sender 执行指令的一方
+     * @param args 参数
+     * @return 是否成功
+     */
     public boolean onCommand(@NotNull CommandSender sender, @NotNull String[] args);
 }
