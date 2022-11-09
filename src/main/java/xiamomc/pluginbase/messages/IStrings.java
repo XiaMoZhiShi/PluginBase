2fbe18a948be5f132a2d906eb2209b4ab2c4fbb8
MATRIX-feather <midnightcarnival@outlook.com>
2022-11-10 01:22:52 +0800

    misc: Bump version rm: 移除不再需要的软件包
    Signed-off-by: MATRIX-feather <midnightcarnival@outlook.com>

--- src/main/java/xiamomc/pluginbase/messages/IStrings.java
+++
@@ -1,9 +0,0 @@
-package xiamomc.pluginbase.messages;
-
-/**
- * @deprecated 转移到了 {@link xiamomc.pluginbase.Messages.IStrings}
- */
-@Deprecated
-public interface IStrings extends xiamomc.pluginbase.Messages.IStrings
-{
-}
